c4442e4da6974d65a3e7179a3241f7d59f2b11fc
CupKido <saarta03@gmail.com>
2024-08-07 22:38:18 +0300

    deleted node modules

--- routes/gptModeRoute.js
+++ routes/gptModeRoute.js
@@ -1,10 +1,14 @@
 
-const { gpt4mode, gpt3mode, start, help, } = require('../controllers/gptModeController');
+const { gpt4turbomode, gpt4omode, dallemode, gpt3mode, start, help, } = require('../controllers/gptModeController');
 
 
 
 module.exports = (bot) => {
-    bot.command('gpt4', gpt4mode);
+    bot.command('gpt4', gpt4turbomode);
+
+    bot.command('gpt4o', gpt4omode);
+
+    bot.command('dalle', dallemode);
 
     bot.command('gpt3', gpt3mode);
 
--- routes/gptRoute.js
+++ routes/gptRoute.js
@@ -1,6 +1,52 @@
-const {gptResponse, gptRecognize} = require('../controllers/gptController');
+const {getMode, gptResponse, gptGenerateImage, gptRecognize, models} = require('../controllers/gptController');
 const { message } = require('telegraf/filters')
+const {LOADING_MESSAGES} = require('../config/const_messages');
+
+const messagePending = [];
+
 module.exports = (bot) => {
-    bot.on(message('text'), gptResponse);
-    bot.on('photo', gptRecognize);
+    bot.on(message('text'), addLoadingMessage(navigateGptMessage));
+    bot.on('photo', addLoadingMessage(gptRecognize));
+}
+
+const navigateGptMessage = async (ctx) =>{
+    const mode = await getMode(ctx);
+
+    console.log(Object.values(models.IMAGE_GENERATION).includes(mode), Object.values(models.IMAGE_GENERATION), mode)
+    if (mode && Object.values(models.IMAGE_GENERATION).includes(mode)) {
+        await gptGenerateImage(ctx);
+    }
+    else {
+        await gptResponse(ctx);
+    }
+}
+
+const addLoadingMessage = (func) => {
+    return async (ctx) => {
+        try {
+            userId = ctx.from.id;
+            if(messagePending.includes(userId)){
+                ctx.reply('waittttt! cant take more than 1 request at a time');
+
+                return;
+            }
+
+            messagePending.push(userId);
+            const loadingMessage = await ctx.reply(LOADING_MESSAGES[Math.floor(Math.random()*LOADING_MESSAGES.length)]);
+
+            func(ctx)
+            .then(() => {
+                ctx.deleteMessage(loadingMessage.message_id);
+                messagePending.splice(messagePending.indexOf(userId), 1);
+            })
+            .catch((error) => {
+                console.log('error', error);
+                ctx.deleteMessage(loadingMessage.message_id);
+                ctx.reply('error');
+            });   
+        }catch(e){
+            console.log('error', e);
+            messagePending.splice(messagePending.indexOf(userId), 1);
+        }
+    }
 }
