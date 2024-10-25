63be37d4a8a0f8d2df53348b2e340ffc321ff86f
CupKido <saarta03@gmail.com>
2024-10-25 22:02:39 +0300

    added model commands

--- routes/gptModeRoute.js
+++ routes/gptModeRoute.js
@@ -1,16 +1,22 @@
 
-const { gpt4turbomode, gpt4omode, dallemode, gpt3mode, start, help, } = require('../controllers/gptModeController');
+const { gpt4turbomode, gpt4omode, dallemode, gpt3mode, start, help, gpto1mode, gpto1minimode, gpt4ominimode } = require('../controllers/gptModeController');
 
 
 
 module.exports = (bot) => {
-    bot.command('gpt4', gpt4turbomode);
+    bot.command('gpt3', gpt3mode);
+    
+    bot.command('gpt4omini', gpt4ominimode)
 
     bot.command('gpt4o', gpt4omode);
-
+    
     bot.command('dalle', dallemode);
+    
+    bot.command('gpt4', gpt4turbomode);
 
-    bot.command('gpt3', gpt3mode);
+    bot.command('o1', gpto1mode);
+
+    bot.command('o1mini', gpto1minimode);
 
     bot.start(start);
 
