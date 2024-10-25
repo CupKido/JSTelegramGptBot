c73abf3ad6842427097b54e6ed74f19114eac340
CupKido <saarta03@gmail.com>
2024-10-25 23:45:55 +0300

    added more admin commands

--- routes/authorizeRoute.js
+++ routes/authorizeRoute.js
@@ -1,8 +1,14 @@
 const UserModel = require('../models/userMode');
-const { authUsers, listUsers } = require('../controllers/authController');
+const { authUsers, listUsers, announce, tell, history } = require('../controllers/authController');
 
 module.exports = (bot) => {
     bot.command('auth', authUsers);
 
     bot.command('listUsers', listUsers);
+
+    bot.command('announce', announce);
+
+    bot.command('tell', tell);
+
+    bot.command('history', history);
 }
