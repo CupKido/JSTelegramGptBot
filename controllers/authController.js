a330dc6feaf0047599b335a9240315f3d61b8703
CupKido <saarta03@gmail.com>
2024-12-25 22:27:33 +0200

    removed auth check from functions

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const UserMode = require('../models/userMode');
-const { doIfAdmin, changeUser, applyOnUsers, getUsersIds } = require('../modules/userManager');
+const { changeUser, applyOnUsers, getUsersIds } = require('../modules/userManager');
 const { splitMessageAndReply } = require('../utils');
 
 const authUser = async (id, value) => {
@@ -10,21 +10,21 @@ const unlimitUser = async (id, value) => {
     await changeUser(id, (usermode) => { usermode.unlimited = value; });
 }
 
-const authUsers = doIfAdmin(applyOnUsers(authUser, "Authed users", true))
-const unlimitUsers = doIfAdmin(applyOnUsers(unlimitUser, "Unlimited users", true))
-const deAuthUsers = doIfAdmin(applyOnUsers(authUser, "DeAuthed users", false))
-const deUnlimitUsers = doIfAdmin(applyOnUsers(unlimitUser, "DeUnlimited users", false))
+const authUsers = applyOnUsers(authUser, "Authed users", true)
+const unlimitUsers = applyOnUsers(unlimitUser, "Unlimited users", true)
+const deAuthUsers = applyOnUsers(authUser, "DeAuthed users", false)
+const limitUsers = applyOnUsers(unlimitUser, "DeUnlimited users", false)
 
-const listUsers = doIfAdmin((ctx, usermode) => {
+const listUsers = (ctx, usermode) => {
     UserMode.find().then((result) => {
         splitMessageAndReply('Users: \n' + result.map((user) =>
              {return `\tid: ${user._id} name: ${user.name} mode: ${user.mode} authorized: ${user.authorized} admin: ${user.admin}\n`}
             ),
         3000, ctx);
     });
-});
+}
 
-const announce = doIfAdmin((ctx, usermode) => {
+const announce = (ctx, usermode) => {
     message = ctx.update.message.text.split(' ').slice(1).join(' ');
     getUsersIds().then((usersIds) => {
         ctx.reply(usersIds)
@@ -34,9 +34,9 @@ const announce = doIfAdmin((ctx, usermode) => {
                 .catch((error) => {console.log(error + '\nunable to send announcement to ' + userId)});
         })
     })
-});
+}
 
-const tell = doIfAdmin((ctx, usermode) => {
+const tell = (ctx, usermode) => {
     splitMessage = ctx.update.message.text.split(' ');
     if(splitMessage.length < 3){
         ctx.reply('please provide a user id and a message');
@@ -46,9 +46,9 @@ const tell = doIfAdmin((ctx, usermode) => {
     message = splitMessage.slice(2).join(' ');
     ctx.reply('telling ' + userId + ' ' + message);
     ctx.telegram.sendMessage(userId, message).then();
-});
+}
 
-const history = doIfAdmin(async (ctx, usermode) => {
+const history = async (ctx, usermode) => {
     splitMessage = ctx.update.message.text.split(' ');
     if(splitMessage.length < 2){
         ctx.reply('please provide a user id');
@@ -63,13 +63,13 @@ const history = doIfAdmin(async (ctx, usermode) => {
             splitMessageAndReply(chatHistory, 3000, ctx);
         }
     });
-});
+}
 
 module.exports = {
     authUsers,
     unlimitUsers,
     deAuthUsers,
-    deUnlimitUsers,
+    limitUsers,
     listUsers,
     announce,
     tell,
--- controllers/gptModeController.js
+++ controllers/gptModeController.js
@@ -1,34 +1,20 @@
-const UserMode = require('../models/userMode');
 const {models} = require('nodegptwrapper');
-const {HELP_MESSAGE, ADMIN_HELP_MESSAGE} = require('../config/const_messages');
-const { swapToMode, doIfInit, doIfAuthed, doIfUnlimited } = require('../modules/userManager');
+const { swapToMode } = require('../modules/userManager');
 
-const gpt3mode = doIfInit(swapToMode(models.CHAT.GPT3))
 
-const gpt4ominimode = doIfInit(swapToMode(models.CHAT.GPT4OMINI));
+const gpt3mode = swapToMode(models.CHAT.GPT3);
 
-const gpt4turbomode = doIfAuthed(swapToMode(models.CHAT.GPT4TURBO));
+const gpt4ominimode = swapToMode(models.CHAT.GPT4OMINI);
 
-const gpt4omode = doIfAuthed(swapToMode(models.CHAT.GPT4O));
+const gpt4turbomode = swapToMode(models.CHAT.GPT4TURBO);
 
-const dallemode = doIfAuthed(swapToMode(models.IMAGE_GENERATION.DALLE3));
+const gpt4omode = swapToMode(models.CHAT.GPT4O);
 
-const gpto1mode = doIfUnlimited(swapToMode(models.CHAT.O1));
+const dallemode = swapToMode(models.IMAGE_GENERATION.DALLE3);
 
-const gpto1minimode = doIfAuthed(swapToMode(models.CHAT.O1MINI));
+const gpto1mode = swapToMode(models.CHAT.O1);
 
-const start = doIfInit((ctx, usermode) => {
-    ctx.reply(HELP_MESSAGE);
-});
-
-const help = doIfInit((ctx, usermode) => {
-    console.log(usermode)
-    if(usermode.admin){
-        ctx.reply(HELP_MESSAGE + '\n' + ADMIN_HELP_MESSAGE);
-    }else{
-        ctx.reply(HELP_MESSAGE);
-    }
-})
+const gpto1minimode = swapToMode(models.CHAT.O1MINI);
 
 module.exports = {
     gpt3mode,
@@ -38,6 +24,4 @@ module.exports = {
     dallemode,
     gpto1mode,
     gpto1minimode,
-    start,
-    help
 }
